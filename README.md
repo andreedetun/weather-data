900ac398da406fe1558d0bed84ccf8ede6baf7cd
andreedetun <andre.edetu.falk@gmail.com>
2019-04-14 21:12:47 +0200

    Final release

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Weather data
 Sends out weather data every morning through sms.
+
+# Api's needed
+https://openweathermap.org/current
+https://www.twilio.com/ (To get a number, costs 2$ per month)
+
+# Config
+```
+{
+    "api-key": <openweathermap api-key>,
+    "twilio-sid": <twilio account id>,
+    "twilio-secret": <twilio secret key>,
+    "number": <twilio number>
+}
+```
--- app.py
+++ app.py
@@ -2,14 +2,20 @@ import os
 import sys
 import requests
 import json
-
+from twilio.rest import Client
+from datetime import datetime
+from threading import Timer
+ 
 # Specify what city to get data about.
 city = sys.argv[1]
 
-# Get api key from config file.
+# Get api key & twilio credentials from config file.
 with open('config.json') as config_file:
     config = json.load(config_file)
     key = config['api-key']
+    sid = config['twilio-sid']
+    secret = config['twilio-secret']
+    number = config['number']
 
 # Format the url used for weather requests correctly.
 url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&APPID={key}'
@@ -35,3 +41,29 @@ max_temp = data['list'][0]['main']['temp_max']
 # This will for example return '-5.4' (celcius)
 min_temp = data['list'][0]['main']['temp_min']
 
+# Authenticate the twilio client.
+client = Client(sid, secret)
+
+def send_report(): 
+# Create the message and send.
+    message = client.messages \
+                    .create(
+                         body=f"Latest forecast for {city}: {general}, {description}, with the current temperature of {cur_temp}\N{DEGREE SIGN}c, max temp: {max_temp}\N{DEGREE SIGN}c, min temp: {min_temp}\N{DEGREE SIGN}c",
+                        from_='+46769448018', # This is the number on twilio
+                        to=number # Number in config file
+                    )
+
+# Time stuff when this will be executed
+cur_time = datetime.today()
+
+send_time = cur_time.replace(day=cur_time.day, hour=21, minute= 7, second=0, microsecond=0)
+
+delta_t = send_time - cur_time
+
+secs = delta_t.seconds + 1
+
+if __name__ == '__main__':
+    timer = Timer(secs, send_report)
+    timer.start()
+
+# TODO: send sms api.
